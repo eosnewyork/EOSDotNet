f2ec86b236fcb38f7dbd927f963a479194d87ca2
Warrick FitzGerald <wfitzgerald@livetechnology.com>
2018-07-01 22:50:25 -0400

    Added nlog logger

--- EOSLib/Class1.cs
+++ EOSLib/Class1.cs
@@ -1,9 +1,16 @@
-﻿using System;
+﻿using NLog;
+using System;
 
 namespace EOSLib
 {
     public class Class1
     {
+        static Logger logger = NLog.LogManager.GetCurrentClassLogger();
         public string test;
+
+        public void testp()
+        {
+            logger.Info("Logged via NLOG");
+        }
     }
 }
--- EOSLibConsole/Program.cs
+++ EOSLibConsole/Program.cs
@@ -8,6 +8,7 @@ namespace EOSLibConsole
         static void Main(string[] args)
         {
             Class1 class1 = new Class1();
+            class1.testp();
             class1.test = "xxxxxxxx";
             Console.WriteLine("Hello World!" + class1.test);
             Console.ReadLine();
